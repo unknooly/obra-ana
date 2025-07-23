e9503ded89b094c8a9288f39f423e2665d609ee6
unknooly <97864953+unknooly@users.noreply.github.com>
2025-07-22 22:53:39 -0300

    inicio script

--- script.js
+++ script.js
@@ -1,11 +1,36 @@
-const q=new Array(10)
+// const q=new Array(10)
 
-function formSend(){
-    const p=q.filter(x=>x===null||x===undefined)
-    console.log(p,typeof(p),p.length)
-    console.log(`Form's been sent:`,q)
-}
+// function formSend(){
+//     const p=q.filter(x=>x===null||x===undefined)
+//     console.log(p,typeof(p),p.length)
+//     console.log(`Form's been sent:`,q)
+// }
 
-function handlechange(id){
-    q[id-1]=document.getElementById(`q${id}`).value
-}
+// function handlechange(id){
+//     q[id-1]=document.getElementById(`q${id}`).value
+// }
+
+document.getElementById("form").addEventListener("submit",function(e){
+    e.preventDefault()
+    const formData=new FormData(this)
+
+    // COM OBJECT:
+    const data={}
+    formData.forEach((value,key)=>{
+        data[key]=value
+    })
+
+    // COM ARRAY:
+    // const data=[]
+    // formData.forEach((value,key)=>{
+    //     data.push({
+    //         value: Number(value),
+    //         result: Number(value)*0.8
+    //     })
+    // })
+    console.log(data)
+
+    const res=document.getElementById("resultado")
+    console.log(res.style)
+    res.style="display: unset"
+})
