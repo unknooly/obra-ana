aa10524ba33efb6207e40ae33424b54426e419e9
unknooly <97864953+unknooly@users.noreply.github.com>
2025-07-25 20:31:22 -0300

    1 questao por pagina + footer

--- script.js
+++ script.js
@@ -1,13 +1,48 @@
 const response = (text,hideForm)=>{
     document.getElementById("output").innerHTML=text
-    document.getElementById("result").style="display:unset;"
-    if(hideForm==="hideForm") document.getElementById("form").style="display:none;"
+    document.getElementById("result").style.display="unset"
+    if(hideForm==="hideForm") document.getElementById("form").style.display="none"
 }
+
+const qtdPerguntas=document.getElementById("questoes").getElementsByTagName("li").length
+
+var currentQuestion=0
+
+const toggleQuestion = (id,show)=>{
+    const el=document.getElementById(id)
+    el.style.display = (show) ?"unset" :"none"
+}
+
+document.getElementById("anterior").addEventListener("click",function(e){
+    e.preventDefault()
+    if(currentQuestion>0){
+        // if(currentQuestion===1) toggleQuestion("anterior",false);
+        // if(currentQuestion===qtdPerguntas) toggleQuestion("proxima",true);
+        if(currentQuestion===1) document.getElementById("anterior").innerHTML="-"
+        if(currentQuestion===qtdPerguntas) document.getElementById("proxima").innerHTML="Próxima"
+
+        toggleQuestion(`questao${currentQuestion}`,false)
+        toggleQuestion(`questao${currentQuestion-1}`,true)
+        currentQuestion-=1
+    }
+})
+
+document.getElementById("proxima").addEventListener("click",function(e){
+    e.preventDefault()
+    if(currentQuestion<qtdPerguntas){
+        // if(currentQuestion===qtdPerguntas-1) toggleQuestion("proxima",false);
+        // if(currentQuestion===0) toggleQuestion("anterior",true);
+        if(currentQuestion===qtdPerguntas-1) document.getElementById("proxima").innerHTML="-"
+        if(currentQuestion===0) document.getElementById("anterior").innerHTML="Anterior"
+        toggleQuestion(`questao${currentQuestion}`,false)
+        toggleQuestion(`questao${currentQuestion+1}`,true)
+        currentQuestion+=1
+    }
+})
 
 document.getElementById("form").addEventListener("submit",function(e){
     e.preventDefault()
 
-    const qtdPerguntas=document.getElementById("questoes").getElementsByTagName("li").length
     const formData=new FormData(this)
     const data=[]
 
@@ -21,6 +56,6 @@ document.getElementById("form").addEventListener("submit",function(e){
         document.getElementById(`resposta${ // diferentes resultados
             (result>-1&&result<8) ? 1 :
             (result<15) ? 2 : 3
-        }`).style="display:unset;"
+        }`).style.display="unset"
     }
 })
